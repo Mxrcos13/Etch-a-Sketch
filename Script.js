6c2a6e6bffc03a6c20603055215a3a59b8625eb5
Marcos Pantoja <marcospantoja21@outlook.com>
2023-06-08 14:52:09 -0700

    added an event listener that colors whatever square is hovered over

--- Script.js
+++ Script.js
@@ -1,6 +1,16 @@
 const grid = document.querySelector('.grid');
+
 for(let i = 0; i < 16 * 16; i++){
     const div = document.createElement('div');
     div.classList.add('square');
     grid.appendChild(div);
 }
+function paint(e){
+    const selector = e.target;
+    selector.style.backgroundColor = 'green';
+}
+
+const squares = document.querySelectorAll('.square');
+squares.forEach(square => {
+    square.addEventListener('mouseover', paint);
+});
